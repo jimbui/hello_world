4e873e16259e159c1dbda1ddee344b44902aa1eb
jimbui <jimbui501@gmail.com>
2016-04-01 08:47:58 -0400

    Update Seat.h

--- Seat.h
+++ Seat.h
@@ -1,18 +1,16 @@
 #pragma once
 #include <string>
-using namespace std;
+using namespace std ;
 
 class Seat
 {
 private:
-	string seat_row_name;
-	int seat_number;
-	bool sold;
+	string seat_row_name ;
+	int seat_number ;
+	bool sold ;
 
 public:
-	Seat(string Row_Name,
-		int Seat_Number);
-
-	void Sell();
-	void Display() const;
-};
+	Seat(string Row_Name , int Seat_Number) ;
+	void Sell() ;
+	void Display() const ;
+} ;
